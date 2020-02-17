0ccc3acca0574d6d708dfd323e18df3500138ee9
Luiz Leite Oliveira <luiz.leite@hotmart.com>
2020-02-17 10:58:43 -0300

    create project from extension of cs go

---
+++ cs-go-where-to-go/src/main/kotlin/com/luizleite/csgowheretogo/CsGoWhereToGoApplication.kt
@@ -0,0 +1,11 @@
+package com.luizleite.csgowheretogo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CsGoWhereToGoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CsGoWhereToGoApplication>(*args)
+}
---
+++ cs-go-where-to-go/src/test/kotlin/com/luizleite/csgowheretogo/CsGoWhereToGoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.luizleite.csgowheretogo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CsGoWhereToGoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
