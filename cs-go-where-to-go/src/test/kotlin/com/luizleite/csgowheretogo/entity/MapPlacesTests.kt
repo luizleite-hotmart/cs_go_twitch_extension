1ab958f12e72a10e34aa7f638d3a0f9e93005857
Luiz Leite Oliveira <luiz.leite@hotmart.com>
2020-02-17 14:58:11 -0300

    create map place test

--- cs-go-where-to-go/src/test/kotlin/com/luizleite/csgowheretogo/entity/MapPlacesTests.kt
+++ cs-go-where-to-go/src/test/kotlin/com/luizleite/csgowheretogo/entity/MapPlacesTests.kt
@@ -1,13 +1,16 @@
 package com.luizleite.csgowheretogo.entity
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class MapPlaces {
+class MapPlacesTests {
 
     @Test
     fun `create map place from scratch`() {
         val mapPlace = MapPlace(name = "key.name", description = "key.description")
+        assertEquals("key.name", mapPlace.name)
+        assertEquals("key.description", mapPlace.description)
     }
 }
